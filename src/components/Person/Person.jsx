b50aac4e4647d183aa4f97b5555f8019dec2bc81
Rodrigo <sistsayap>
2026-05-15 17:42:09 -0300

    fix: correct Navigation import

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,17 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({
+  person: { name, age, sex, isMarried, partnerName },
+}) => {
+  const partnerPhrase = isMarried
+    ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+    : 'I am not married';
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">{name}</h2>
+      {age && <p className="Person__age">Age: {age}</p>}
+      <p className="Person__partner">{partnerPhrase}</p>
+    </div>
+  );
+};
