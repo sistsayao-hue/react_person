6334114e28fda37ea5f4ecc943751cbc2dcda87c
Rodrigo <sistsayao@gmail.com>
2026-05-20 07:57:19 -0300

    Update Person.jsx

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 export const Person = ({
   person: { name, age, sex, isMarried, partnerName },
 }) => {
+  const partnerRole = sex === 'm' ? 'wife' : 'husband';
   const partnerPhrase = isMarried
-    ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+    ? `${partnerName} is my ${partnerRole}`
     : 'I am not married';
 
   return (
     <div className="Person">
-      <h2 className="Person__name">{name}</h2>
-      {age && <p className="Person__age">Age: {age}</p>}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">{partnerPhrase}</p>
     </div>
   );
